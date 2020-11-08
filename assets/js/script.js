b1880cadfe123a87bd66bd58767e67effe36c9e9
Andrena Juric <ajuric.uw@gmail.com>
2020-11-07 17:54:58 -0800

    fixed the working links, added time blocks, got moment.js to auto-update currentDay

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Get the current day displayed in jumbotron header
+var currentDay = moment();
+$("#currentDay").text(currentDay.format("dddd, MMMM Do"));
+
+var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+for (var i = 0; i < hours.length; i++) {
+
+}
